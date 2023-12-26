96988949fbf4155dcba4ceb0e4b04c5b1ec39281
KazazaevDaniil <146122180+KazazaevDaniil@users.noreply.github.com>
2023-12-26 13:38:52 +0300

    Update task6.cpp

--- Lab3/task6.cpp
+++ Lab3/task6.cpp
@@ -1,25 +1,25 @@
-//#include "iostream"
-//#include "vector"
-//
-//using namespace std;
-//
-//vector<int> prices;
-//int ArrSize, vars, FirstBuy, FirstSell = 0, SecondBuy, SecondSell = 0;
-//
-//int main() {
-//    cout << "Razmer massiva\n";
-//    cin >> ArrSize;
-//    cout << "Chisla\n";
-//    for (int Nums = 0; Nums < ArrSize; ++Nums) {
-//        cin >> vars;
-//        cout << endl;
-//        prices.push_back(vars);
-//    }
-//    for (int price = 0; price < ArrSize; ++price) {
-//        FirstBuy = min(FirstBuy,price);
-//        FirstSell = max(FirstSell, price - FirstBuy);
-//        SecondBuy = min(SecondBuy, price - FirstSell);
-//        SecondSell = max(SecondSell, price - SecondBuy);
-//    }
-//}
-//
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+vector<int> prices;
+int ArrSize, vars, FirstBuy, FirstSell = 0, SecondBuy, SecondSell = 0;
+
+int main() {
+   cout << "Razmer massiva\n";
+   cin >> ArrSize;
+   cout << "Chisla\n";
+   for (int Nums = 0; Nums < ArrSize; ++Nums) {
+       cin >> vars;
+       cout << endl;
+       prices.push_back(vars);
+   }
+   for (int price = 0; price < ArrSize; ++price) {
+       FirstBuy = min(FirstBuy,price);
+       FirstSell = max(FirstSell, price - FirstBuy);
+       SecondBuy = min(SecondBuy, price - FirstSell);
+       SecondSell = max(SecondSell, price - SecondBuy);
+   }
+}
+
