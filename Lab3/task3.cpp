4f5f3696cdfaabec69df0be27cb36c93a4ad96c7
KazazaevDaniil <146122180+KazazaevDaniil@users.noreply.github.com>
2023-12-26 13:38:22 +0300

    Update task3.cpp

--- Lab3/task3.cpp
+++ Lab3/task3.cpp
@@ -1,36 +1,36 @@
-//#include "iostream"
-//#include "vector"
-//
-//using namespace std;
-//vector <int> arr;
-//class MedianFinder {
-//public:
-//    void addNum(int nums){
-//        arr.push_back(nums);
-//        cout << "null" << '\n';
-//
-//    };
-//    double findMedian(){
-//        if (size(arr) % 2 != 0){
-//            int middle1 = (size(arr)/2)+1;
-//            cout << middle1 << '\n';
-//        }
-//        else{
-//            double middle2 = (((double(size(arr)/2+1))) + double(size(arr)/2))/2;
-//            cout << double(middle2) << '\n';
-//        }
-//        return 0;
-//    };
-//};
-//
-//
-//int main(){
-//    int nums;
-//    MedianFinder medianFinder;
-//    medianFinder.addNum(1);
-//    medianFinder.addNum(2);
-//    medianFinder.findMedian();
-//    medianFinder.addNum(3);
-//    medianFinder.findMedian();
-//    return 0;
-//}
+#include "iostream"
+#include "vector"
+
+using namespace std;
+vector <int> arr;
+class MedianFinder {
+public:
+   void addNum(int nums){
+       arr.push_back(nums);
+       cout << "null" << '\n';
+
+   };
+   double findMedian(){
+       if (size(arr) % 2 != 0){
+           int middle1 = (size(arr)/2)+1;
+           cout << middle1 << '\n';
+       }
+       else{
+           double middle2 = (((double(size(arr)/2+1))) + double(size(arr)/2))/2;
+           cout << double(middle2) << '\n';
+       }
+       return 0;
+   };
+};
+
+
+int main(){
+   int nums;
+   MedianFinder medianFinder;
+   medianFinder.addNum(1);
+   medianFinder.addNum(2);
+   medianFinder.findMedian();
+   medianFinder.addNum(3);
+   medianFinder.findMedian();
+   return 0;
+}
